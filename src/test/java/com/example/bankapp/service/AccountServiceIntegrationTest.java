57c053daadb71ebb55dac21d792ccd8e00a63bf4
Devansh Pandey <Devanshpandey958@gmail.com>
2025-06-30 05:16:45 +0000

    Testing in CI with H2 DB

--- src/test/java/com/example/bankapp/service/AccountServiceIntegrationTest.java
+++ src/test/java/com/example/bankapp/service/AccountServiceIntegrationTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional
 public class AccountServiceIntegrationTest {
