bd2fd70ea2949f02cee4648fb9e2e33864352613
Devansh Pandey <Devanshpandey958@gmail.com>
2025-08-07 10:05:26 +0000

    Cleaner code

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.bankapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 // import org.springframework.test.context.ActiveProfiles;
-// import org.springframework.test.context.TestPropertySource;
+
 
 import jakarta.transaction.Transactional;
 
@@ -11,10 +11,13 @@ import jakarta.transaction.Transactional;
 @Transactional
 // @TestPropertySource(locations = "classpath:application-test.properties")
 
-public class BankappApplicationTests {
+class BankappApplicationTests {
 
     @Test
     void contextLoads() {
+        // This test will simply check if the application context loads successfully.
+        // If there are any issues with the configuration, this test will fail.
+        // No additional assertions are needed here.
     }
 }
 
